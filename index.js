9c755c896d5f9f83e21e0d54069d0ac34aca547d
Travis <travis.williams94@gmail.com>
2023-03-11 14:02:32 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(c,d){
+    return c - d;
+}
+
+function   multiply(e,f){
+    return e*f;
+}
+
+function divide(g,h){
+    return g/h;
+}
+
+function increment(n){
+    return n += 1;
+}
+
+function decrement(n){
+    return n -= 1;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
